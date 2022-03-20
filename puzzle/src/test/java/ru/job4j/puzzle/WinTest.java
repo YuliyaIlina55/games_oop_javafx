bff2d6e49942ec2558096f062bd15f83dd3f96b4
Yuliya Ilina <flow987876765@gmail.com>
2022-03-20 23:03:58 +0900

    Corrected file WinTest

--- puzzle/src/test/java/ru/job4j/puzzle/WinTest.java
+++ puzzle/src/test/java/ru/job4j/puzzle/WinTest.java
@@ -1,6 +1,6 @@
 package ru.job4j.puzzle;
 
-import org.junit.Ignore;
+
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
@@ -8,7 +8,6 @@ import static org.junit.Assert.*;
 
 
 public class WinTest {
-
     @Test
     public void whenVerticalWin() {
         int[][] board = {
